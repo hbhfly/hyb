b1d638968ee4c715dacc0775f94006ec15fd63e6
i323130 <wallace20130520@163.com>
2021-10-11 11:04:41 +0800

    path maybe wrong

---
+++ core-customize/multicloudhotfolder/bin/custom/multicloudhotfolder/src/com/hybris/es/cloud/azure/hotfolder/MultiZipHeaderSetupTask.java
@@ -0,0 +1,111 @@
+package com.hybris.es.cloud.azure.hotfolder;
+
+import de.hybris.platform.cloud.hotfolder.dataimport.batch.zip.ZipBatchHeader;
+
+import java.io.File;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.hybris.platform.util.CSVConstants;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.messaging.Message;
+import org.springframework.util.Assert;
+import de.hybris.platform.util.Config;
+
+/*
+    Replace default ZipHeaderSetupTask class
+    Initially setup the batch header. The header is used throughout the pipeline as a reference and for cleanup.
+
+    This customization allows multi catalog management by fetching catalog name through regex pattern
+ */
+public class MultiZipHeaderSetupTask {
+        private static final Logger LOG = LoggerFactory.getLogger(MultiZipHeaderSetupTask.class);
+
+        private File unzippedRootDirectory;
+        private String headerKeyFile;
+        private String headerKeyUnzippedFolder;
+        private String vendorPattern;
+        private boolean net;
+
+        public ZipBatchHeader execute(final Message<?> message)
+        {
+            Assert.isTrue(message.getPayload() instanceof Map, "message.payload to be of type Map.class");
+            final Map<Object, File> unzippedFiles = (Map) message.getPayload();
+
+            final String fileName = (String) message.getHeaders().get(headerKeyFile);
+            Assert.notNull(fileName, "message.headers should contain '" + headerKeyFile + "'");
+            final String explodedFolderName = fileName.substring(0, fileName.length() - 4);
+
+            final String unzippedFolderName = message.getHeaders().get(headerKeyUnzippedFolder).toString();
+            Assert.notNull(unzippedFolderName, "message.headers should contain '" + headerKeyUnzippedFolder + "'");
+            final File unzippedAs = new File(this.unzippedRootDirectory, String.join(File.separator, unzippedFolderName, explodedFolderName));
+
+            final ZipBatchHeader header = new ZipBatchHeader();
+            header.setOriginalFileName(fileName);
+            header.setFileUnzippedAs(unzippedAs);
+            header.setUnzippedFiles(unzippedFiles.values());
+            header.setEncoding(CSVConstants.HYBRIS_ENCODING);
+            header.setCatalog(extractCatalogID(fileName));
+            header.setNet(net);
+            return header;
+        }
+
+        private String extractCatalogID(String filename) {
+            Pattern pattern = Pattern.compile(vendorPattern);
+            Matcher matcher = pattern.matcher(filename);
+            String catalog = "";
+            if (matcher.find())
+            {
+                String venderId = matcher.group(1);
+                catalog = Config.getParameter("cloud.hotfolder." + venderId + ".zip.header.catalog");
+                LOG.info("Catalog name extracted [{}] ", catalog);
+            } else {
+                catalog = Config.getParameter("cloud.hotfolder.default.zip.header.catalog");
+                LOG.info("Catalog default name extracted [{}] ", catalog);
+            }
+            return catalog;
+        }
+
+        /**
+         * Set the root directory of where the zip was extracted into
+         */
+        @Required
+        public void setUnzippedRootDirectory(final File unzippedRootDirectory)
+        {
+            this.unzippedRootDirectory = unzippedRootDirectory;
+        }
+
+        /**
+         * Set the header key to use to get a reference to the original file
+         */
+        @Required
+        public void setHeaderKeyFile(final String headerKeyFile)
+        {
+            this.headerKeyFile = headerKeyFile;
+        }
+
+        /**
+         * Set the header key to use to get the name of the folder the file was extracted into
+         */
+        @Required
+        public void setHeaderKeyUnzippedFolder(final String headerKeyUnzippedFolder)
+        {
+            this.headerKeyUnzippedFolder = headerKeyUnzippedFolder;
+        }
+
+        @Required
+        public void setVendorPattern(final String vendorPattern)
+        {
+            this.vendorPattern = vendorPattern;
+        }
+
+        @Required
+        public void setNet(final boolean net)
+        {
+            this.net = net;
+        }
+    }
